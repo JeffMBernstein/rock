ebe08494c9d49a00570e766e757093879dda303c
Jeff Bernstein <jeffreymbernstein@gmail.com>
2013-07-05 17:04:20 +0000

    Rock git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week1Quiz::Application.config.secret_key_base = '07c2568ef438323c316d8b4df16388cc3588e768e72b2f6d7919fad4323bbdda40b723f9f8a1332b78ace42c3b92fe98526fde60c789dd1db143d93df95455f0'
